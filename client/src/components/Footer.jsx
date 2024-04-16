01471e1a719dea31bc8e9ec06a82c65b0a426a47
ANTOINE BERTHELOT <ANTOINE.BERTHELOT65@GMAIL.COM>
2024-04-16 17:49:18 +0200

    feat: create footer.jsx, update all css

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="https://www.artic.edu/">About us</a>
+        </li>
+        <li>
+          <a href="https://www.artic.edu/">Contact</a>
+        </li>
+        <li>
+          Images courtesy
+          <a href="https://www.artic.edu/">Art Institute of Chicago</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,20 +4,20 @@ function Navbar() {
   return (
     <nav className="navbar">
       <img
-        className="navbar-logo"
+        className="navbarLogo"
         src="/src/assets/images/logo300.webp"
         alt="logo Art from home"
       />
 
-      <ul className="navbar-links">
+      <ul className="navbarList">
         <li className="navbar-item">
-          <a href="/" className="navbar-link">
+          <a href="/" className="navbarLink">
             Accueil
           </a>
         </li>
         <li>
           <a href="/">
-            exhibition rooms <span>↓</span>{" "}
+            exhibition rooms <span className="arrow">↓</span>{" "}
           </a>
         </li>
       </ul>
