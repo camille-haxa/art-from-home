609958843b09b1263f0b0b6f0cd9ced212dccb94
ANTOINE BERTHELOT <ANTOINE.BERTHELOT65@GMAIL.COM>
2024-04-24 09:44:03 +0200

    fix: update route server for display art page

--- client/src/pages/DisplayArtPage.jsx
+++ client/src/pages/DisplayArtPage.jsx
@@ -2,5 +2,13 @@ import { useLoaderData } from "react-router-dom";
 
 export default function DisplayArtPage() {
   const artsData = useLoaderData();
-  return <div>{artsData}</div>;
+  const { name, description, image } = artsData;
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img src={image} alt="" />
+      <p>{description}</p>
+    </div>
+  );
 }
