7b50e4f9266ff34314b3eeb2aa826e1a6d084421
camille-haxa <camille.haxa@proton.me>
2024-04-25 14:56:18 +0200

    feat: create filters, add nav link from filter to category art page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,12 @@
 import CollectionPage from "../pages/CollectionPage";
+import Filters from "./Filters";
 import WelcomeCard from "./WelcomeCard";
 
 function Home() {
   return (
     <div>
       <WelcomeCard />
+      <Filters />
       <CollectionPage />
     </div>
   );
